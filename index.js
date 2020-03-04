1fa52c8d2485b2fc65e22d3b807874df59892dc3
Ilja B <49001925+iljucha@users.noreply.github.com>
2020-03-04 21:22:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+// https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Math/math.random
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min), max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min +1)) + min
+}
+
+function VUID(length = 32) {
+    let charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+    let variety = charset.length
+    let datetime = new Date().toISOString("de-DE").replace(/T|Z/gmi, " ").trim()
+    let timestamp = "", date = datetime.replace(/\D/g, " ").split(" ")
+    let i = 0, dlength = date.length, random = "", rest = 0
+    function encode(int) {
+        let s = []
+        if (int === 0) {
+            return 0
+        }
+        while (int > 0) {
+            s = [charset[int % variety], ...s]
+            int = Math.floor(int / variety)
+        }
+        return s.join("")
+    }
+    for (i; i < dlength; i++) {
+        timestamp += `${encode(date[i])}`
+    }
+    rest = length - timestamp.length
+    if (rest > 0) {
+        random = encode(getRandomIntInclusive(0, Math.pow(variety, rest) - 1)).padStart(rest, "0")
+    }
+    return `${timestamp}${random}`
+}
+
+module.exports = VUID
